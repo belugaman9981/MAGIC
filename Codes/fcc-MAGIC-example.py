ac1a388d044a234ba402e1f64cdd4c0fc4ed112c
belugaman9981 <littledage@outlook.com>
2026-01-04 16:44:55 -0800

    flamefrags sucks

--- Codes/fcc-MAGIC-example.py
+++ Codes/fcc-MAGIC-example.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym",
         "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
 
-df = pd.read_csv("magic04.data", names= cols, nrows=5)
+df = pd.read_csv("magic04.data", names= cols)
 df.head()
+df["class"] = (df["class"] == "g").astype(int)
 
+print(df)
