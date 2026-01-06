1df9cc07738e8abaad853b5941c69753f848c623
belugaman9981 <littledage@outlook.com>
2026-01-05 17:12:00 -0800

    Update fcc-MAGIC-example.py

--- Codes/fcc-MAGIC-example.py
+++ Codes/fcc-MAGIC-example.py
@@ -15,7 +15,11 @@ print(df)
 for label in cols:
         plt.hist(df[df["class"] == 1][label], color= 'blue', alpha=0.7, label='gamma', density= True)
         plt.hist(df[df["class"] == 0][label], color= 'red', alpha=0.7, label='hadron', density= True)
-        
+        plt.title(label)
+        plt.ylabel("Probability")
+        plt.xlabel(label)
+        plt.legend()
+        plt.show()
         
 # Well
 
