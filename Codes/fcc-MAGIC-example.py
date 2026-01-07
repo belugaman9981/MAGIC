1db7b0e8a5661e34f44a38977f027304d5bdfe14
belugaman9981 <littledage@outlook.com>
2026-01-06 18:03:32 -0800

    Update fcc-MAGIC-example.py

--- Codes/fcc-MAGIC-example.py
+++ Codes/fcc-MAGIC-example.py
@@ -12,7 +12,7 @@ df["class"] = (df["class"] == "g").astype(int)
 
 print(df)
 
-for label in cols:
+for label in cols[:-1]:
         plt.hist(df[df["class"] == 1][label], color= 'blue', alpha=0.7, label='gamma', density= True)
         plt.hist(df[df["class"] == 0][label], color= 'red', alpha=0.7, label='hadron', density= True)
         plt.title(label)
