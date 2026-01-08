fdeacffd708740a5d3488fee583bac4612e8cbb5
belugaman9981 <littledage@outlook.com>
2026-01-07 18:57:31 -0800

    Update fcc-MAGIC-example.py
    Co-Authored-By: belugamanAlt <253176396+belugaman9981alt1@users.noreply.github.com>

--- Codes/fcc-MAGIC-example.py
+++ Codes/fcc-MAGIC-example.py
@@ -56,7 +56,7 @@ test, X_test, y_test    = scale_dataset(test, oversample= False)
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics   import classification_report, confusion_matrix
 
-knn_model = KNeighborsClassifier(n_neighbors= 1)
+knn_model = KNeighborsClassifier(n_neighbors= 5)
 knn_model.fit(X_train, y_train)
 
 y_pred = knn_model.predict(X_test)
