0d959a29b0a42f99ec5dc256bad0ff9dd8e6b8dc
belugaman9981 <littledage@outlook.com>
2026-01-09 19:21:26 -0800

    Update fcc-MAGIC-example.py
    Co-Authored-By: belugamanAlt <253176396+belugaman9981alt1@users.noreply.github.com>

--- Codes/fcc-MAGIC-example.py
+++ Codes/fcc-MAGIC-example.py
@@ -69,3 +69,16 @@ from sklearn.naive_bayes import GaussianNB
 nb_model = GaussianNB()
 nb_model.fit(X_train, y_train)
 
+y_pred = nb_model.predict(X_test)
+print(classification_report(y_test, y_pred))
+
+# talking about Logistic Regression
+
+
+# Logistic Regression
+
+from sklearn.linear_model import LogisticRegression
+
+lg_model = LogisticRegression(max_iter= 1000)
+lg_model = lg_model.fit(X_train, y_train)
+
