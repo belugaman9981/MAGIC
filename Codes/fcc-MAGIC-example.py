ec9687ab8cec31b245eb6a48166074ce647bedd3
belugaman9981 <littledage@outlook.com>
2026-01-12 20:28:18 -0800

    Update fcc-MAGIC-example.py

--- Codes/fcc-MAGIC-example.py
+++ Codes/fcc-MAGIC-example.py
@@ -168,3 +168,7 @@ for num_nodes in [16, 32, 64]:
                             
 
 
+y_pred = least_val_model.predict(X_test)
+y_pred = (y_pred > 0.5).astype(int).reshape(-1)
+
+print(classification_report(y_test, y_pred))
